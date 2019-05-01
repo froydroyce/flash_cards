0805036d353e358aba68768a665f1e35a30faefe
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-01 14:31:03 -0600

    Get cards_in_category method working

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,19 @@
 class Deck
   attr_reader :cards
+
   def initialize(cards)
     @cards = cards
   end
 
   def cards_in_category(category)
-    @cards(category)
+    categorized_cards = []
+      cards.each do |card|
+        if card.category == category
+          categorized_cards.push(card)
+        end
+      end
+      categorized_cards
   end
-
 
   def count
     @cards.count
