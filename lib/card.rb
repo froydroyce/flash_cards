3741da738413edeb96538877ef5d975957d6e683
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-04-30 21:10:14 -0600

    Add card.rb and turn.rb

---
+++ lib/card.rb
@@ -0,0 +1,23 @@
+class Card
+	attr_reader :question, :answer, :catergory
+	def initialize(question, answer, category)
+		@question = question
+		@answer = answer
+		@category = category
+	end
+
+	def question
+		@question
+	end
+
+	def answer
+		@answer
+	end
+
+	def category 
+		@category
+	end
+	
+end
+
+ 
---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+class Turn
+	attr_reader :guess, :card
+	def initialize(guess, card)
+		@guess = guess
+		@card = card
+ 	end
+
+ 	def card
+ 		@card
+ 	end
+
+
+	def guess
+		@guess
+
+	end
+
+	def correct?
+		@guess == @card.answer
+	end
+
+	def feedback
+		if correct? == true
+			"Correct!"
+		else
+			"Incorrect."
+		end
+	end
+end
+
+	
+
+
+
+
