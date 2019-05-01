c94c2af2bc07a0c84b4b07ac01d7142d83d06a12
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-01 14:32:19 -0600

    Refactor turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,31 +5,15 @@ class Turn
 		@card = card
  	end
 
- 	def card
- 		@card
- 	end
-
-
-	def guess
-		@guess
-
-	end
-
 	def correct?
 		@guess == @card.answer
 	end
 
 	def feedback
-		if correct? == true
+		if correct?
 			"Correct!"
 		else
 			"Incorrect."
 		end
 	end
 end
-
-	
-
-
-
-
