df5d6061659e070df7f35c3bbb946f2929e11b57
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-01 22:32:27 -0600

    Add take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -9,6 +9,13 @@ class Round
     @deck.cards.first
   end
 
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns.push(new_turn)
+    @deck.cards.rotate!
+    new_turn
+  end
+
 
 
 end
