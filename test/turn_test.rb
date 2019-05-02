84cd81a5241c5d1f12ec8cbeb9ea474cc96e2812
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-02 15:33:29 -0600

    Add test_if_it_has_a_guess

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -11,6 +11,10 @@ class TurnTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Turn, @turn
-
   end
+
+  def test_it_has_a_guess
+    assert_equal 'Juneau', @turn.guess
+  end
+
 end
