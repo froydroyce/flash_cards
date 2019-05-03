e085f65e132443ffd2d2ca0ef508c77d33cf3912
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-03 16:59:18 -0600

    Add new_turn and correct? test

--- test/round_test.rb
+++ test/round_test.rb
@@ -2,22 +2,25 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require '/lib/deck'
+require './lib/deck'
 require './lib/round'
-require './lib/'
+require './lib/turn'
+#require 'pry'
 
 class RoundTest < MiniTest::Test
   def setup
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @deck = Deck.new([card_1, card_2, card_3])
-    @round = Round.new(deck)
-    @new_turn = Round.take_turn("Juneau")
+    cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(cards)
+
+    @round = Round.new(@deck)
+
   end
 
   def test_it_exists
-    assert_instance_of @round.deck, [@card_1, @caed_2, @card_3]
+    assert_instance_of Deck, @round.deck
   end
 
   def test_turns_starts_empty
@@ -25,9 +28,18 @@ class RoundTest < MiniTest::Test
   end
 
   def test_it_has_current_card
-    assert_equal @round.current_card, @card_1
+    assert_equal @card_1, @round.current_card
   end
 
-  def method_name
-
+  def test_if_new_turn_exists
+    new_turn = @round.take_turn("Juneau")
+    assert_instance_of Turn, new_turn
   end
+
+  def test_if_guess_is_correct
+      new_turn = @round.take_turn("Juneau")
+      assert new_turn.correct?
+  end
+
+
+end
