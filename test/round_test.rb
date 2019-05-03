fab96d1ffc73c55da0fa45bbf5533c8b87ff8bc8
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-03 17:08:50 -0600

    Add test_if_round_has_turns

--- test/round_test.rb
+++ test/round_test.rb
@@ -12,8 +12,8 @@ class RoundTest < MiniTest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [@card_1, @card_2, @card_3]
-    @deck = Deck.new(cards)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
 
     @round = Round.new(@deck)
 
@@ -37,9 +37,15 @@ class RoundTest < MiniTest::Test
   end
 
   def test_if_guess_is_correct
-      new_turn = @round.take_turn("Juneau")
-      assert new_turn.correct?
+    new_turn = @round.take_turn("Juneau")
+    assert new_turn.correct?
   end
 
+  def test_if_round_has_turns
+    new_turn = @round.take_turn("Juneau")
+    assert_equal new_turn, @round.turns.first
+  end
+
+  def 
 
 end
