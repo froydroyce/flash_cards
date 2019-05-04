dfd8ec0cb489463363c8d118774d9cc4a7f29007
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-04 16:07:43 -0600

    Add card_box and card counter

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,44 +3,70 @@ require './lib/deck'
 require './lib/round'
 require './lib/turn'
 require 'tty-box'
+require 'pry'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
+@card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+@card_2 = Card.new("What is the capital of South Korea?", "Seoul", :Geography)
+@card_3 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+@card_4 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+@deck = Deck.new([@card_1, @card_2, @card_3, @card_4])
+@round = Round.new(@deck)
 
 box_intro = TTY::Box.frame(
   width: 50,
   height: 15,
-  top: 5,
-  left: 10,
+
   align: :center,
-  padding: 3,
+  padding: 5,
   title: {top_left: 'Flashcards with Ruby'}
 ) do
   "Welcome!
   You're playing with 4 cards.
-  Press any key to conitinue."
+  Press Enter to conitinue."
 end
-
-card_box_1 = TTY::Box.frame(
-  width: 50,
-  height: 15,
-  top: 5,
-  left: 10,
-  align: :center,
-  padding: 3,
-  title: {top_left: 'Flashcards with Ruby', bottom_right: 'Card 1 of 4'}
-) do
-  card_1.question
-end
-
-
-
 
 puts box_intro
-
-
-intro = gets.chomp
-
+intro = gets.strip
 if intro.empty?
-  puts card_box_1
+
+  card_box = TTY::Box.frame(
+    width: 50,
+    height: 15,
+
+    align: :center,
+    padding: 5,
+    title: {top_left: 'Flashcards with Ruby', bottom_right: "Card #{@round.turns.count + 1} of 4"}
+  ) do
+    @round.current_card.question
+  end
+
+  puts card_box
+  puts "Your answer?"
+  guess = gets.chomp.capitalize
+  new_turn = @round.take_turn(guess)
+  puts @round.turns.last.feedback
+
+  card_box = TTY::Box.frame(
+    width: 50,
+    height: 15,
+
+    align: :center,
+    padding: 5,
+    title: {top_left: 'Flashcards with Ruby', bottom_right: "Card #{@round.turns.count + 1} of 4"}
+  ) do
+    @round.current_card.question
+  end
+
+  puts card_box
+  guess = gets.chomp.capitalize
+  @round.take_turn(guess)
+  puts @round.turns.last.feedback
 end
+
+
+
+
+#puts "Your answer?"
+#guess = gets.chomp.capitalize
+#@round.take_turn(guess)
+#puts @round.turns.last.feedback
