c37a9bec04f5cb62134826bdf5e6e2358abe5577
Sejin Kim <sejinkim@Sejins-MacBook-Pro.local>
2019-05-04 14:05:56 -0600

    Add card 1

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,13 +4,43 @@ require './lib/round'
 require './lib/turn'
 require 'tty-box'
 
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+
 box_intro = TTY::Box.frame(
-  width: 30,
-  height: 10,
+  width: 50,
+  height: 15,
+  top: 5,
+  left: 10,
   align: :center,
-  padding: 3
+  padding: 3,
+  title: {top_left: 'Flashcards with Ruby'}
 ) do
-  "Welcome to Flashards with Ruby!"
+  "Welcome!
+  You're playing with 4 cards.
+  Press any key to conitinue."
 end
 
-print box_intro
+card_box_1 = TTY::Box.frame(
+  width: 50,
+  height: 15,
+  top: 5,
+  left: 10,
+  align: :center,
+  padding: 3,
+  title: {top_left: 'Flashcards with Ruby', bottom_right: 'Card 1 of 4'}
+) do
+  card_1.question
+end
+
+
+
+
+puts box_intro
+
+
+intro = gets.chomp
+
+if intro.empty?
+  puts card_box_1
+end
